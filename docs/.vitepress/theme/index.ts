cae4ee88e1f659a2999b95f6780f1396662ff414
qlxxkj <admin@qlxxkj.eu.org>
2026-08-23 14:57:36 +0800

    fix: simplify theme, remove problematic language switch component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,3 @@
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
 
-import LanguageSwitch from './LanguageSwitch.vue'
-
-// Add language switch to layout
-const OriginalTheme = DefaultTheme
-const originalSetup = OriginalTheme.setup
-OriginalTheme.setup = function(...args) {
-  if (originalSetup) originalSetup(...args)
-}
-
-export default OriginalTheme
+export default DefaultTheme
